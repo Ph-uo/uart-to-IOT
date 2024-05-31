d235bb54ba06d5d1e056ebe404eff578267892a8
Ph-uo <113018470+Ph-uo@users.noreply.github.com>
2024-05-31 14:41:10 +0700

    Add files via upload

--- esp client.py
+++ esp client.py
@@ -1,41 +1,44 @@
 import asyncio
 import websockets
 import json
+import random
+
+y=0
+x=9
 
 def extrInt(message) :
-    global x,y
+    global ex,ey
     python_data = json.loads(message)
 
-    x = int(python_data["temperature"])
-    y = int(python_data["humidity"])
+    ex = int(python_data["ex"])
+    ey = int(python_data["ey"])
     
-    print(type(x))
-    print(x)
-    print(type(y))
-    print(y)
+    print(type(ex),ex,' & ',type(ey),ey)
 
-async def receive_data():
-    url = "ws://192.168.4.1/ws" 
-
+async def data_t_r():
+    url = "ws://192.168.100.16/ws" 
+    global x,y
     try:
         async with websockets.connect(url) as websocket:
             print("Connected to the WebSocket server")
 
             while True:
+                #data2send = {"x": x,
+                #           'y':y}  
+                #await websocket.send(json.dumps(data2send))
                 message = await websocket.recv()
                 print(f"Received message: {message}")
                 extrInt(message)
-
+    
     except websockets.exceptions.ConnectionClosed as e:
         print(f"Connection closed with exception: {e}")
     except asyncio.TimeoutError:
         print("Connection attempt timed out")
 
 
-if __name__ == "__main__":
-    asyncio.run(receive_data())
-    #extrInt(message)        
-
+asyncio.run(data_t_r())
+x=100
+y=50
 
 
 
